82e1be33c3a342c78e833b7811cd803e381ab391
gaspardousson <gsapardousson>
2021-09-26 15:22:42 +0200

    Add ability to read .cnf files

--- main.ml
+++ main.ml
@@ -2,4 +2,4 @@ open Translater;;
 open Satsolver;;
 
 let f = [| [| -1 |]; [| 1 ; 2 |] |];;
-print_endline (string_of_bool (naive_solver f).(0));;
+print_endline (string_of_bool (naive_solver (read_cnf "test/UF020.91/uf20-01.cnf")).(0));;
