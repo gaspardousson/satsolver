c9c3b2c27ab1015fe92fb69fcdd9f997fc955a2c
gaspardousson <gsapardousson>
2021-09-26 21:07:09 +0200

    Add python script for complexity measures

--- main.ml
+++ main.ml
@@ -1,5 +1,7 @@
 open Translater;;
 open Satsolver;;
 
-let f = [| [| -1 |]; [| 1 ; 2 |] |];;
-print_endline (string_of_bool (naive_solver (read_cnf "test/UF020.91/uf20-01.cnf")).(0));;
+let () =
+    let path = Sys.argv.(1) in
+    print_endline (string_of_bool (naive_solver(read_cnf(path))).(0))
+;;
--- measure.py
+++ measure.py
@@ -13,42 +13,35 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1, length=10
         print()
 
 
-def test(sat, n_lits, n_clauses, n_files):
+def test(path, n):
     os.system("touch test.txt")
     os.system("rm test.txt")
     os.system("touch test.txt")
     start_time = time.time()
-    if sat:
-        for i in range(1, n_files+1):
-            if n_lits < 100:
-                os.system("./satsolver-opt test/UF0" + str(n_lits) + "." + str(n_clauses) + "/uf" + str(n_lits) + "-0" + str(i) + ".cnf >> test.txt")
-            else:
-                os.system("./satsolver-opt test/UF" + str(n_lits) + "." + str(n_clauses) + "/uf" + str(n_lits) + "-0" + str(i) + ".cnf >> test.txt")
-    else:
-        for i in range(1, n_files+1):
-            if n_lits < 100:
-                os.system("./satsolver-opt test/UUF0" + str(n_lits) + "." + str(n_clauses) + "/uf" + str(n_lits) + "-0" + str(i) + ".cnf >> test.txt")
-            else:
-                os.system("./satsolver-opt test/UUF" + str(n_lits) + "." + str(n_clauses) + "/uf" + str(n_lits) + "-0" + str(i) + ".cnf >> test.txt")
-    print(time.time() - start_time)
+    for i in range(1, n+1):
+        os.system("./satsolver-opt " + path + str(i) + ".cnf >> test.txt")
+    return time.time() - start_time
 
 
 def measure(files):
-    os.system("touch measures.txt")
-    os.system("rm measures.txt")
-    os.system("touch measures.txt")
-    print_progress(0, len(files), prefix='Progress:', suffix='Complete', length=50)
+    os.system("touch measure.txt")
+    os.system("rm measure.txt")
+    os.system("touch measure.txt")
+    k = 0
     for f in files:
-        os.system(str(f[1]) + "-" + str(test(f[0], f[1], f[2], f[3])) + "/" + str(f[3]) + "_" + " >> measures.txt")
+        print_progress(k, len(files), prefix='Progress:', suffix='Complete', length=50)
+        os.system("echo " + str(f[2]) + "-" + str(test(f[0], f[1]) / f[1]) + " >> measure.txt")
+        k += 1
+    print_progress(len(files), len(files), prefix='Progress:', suffix='Complete', length=50)
 
 
 def coord_from_file(path, taken=0, unit=6):
     x, y = [], []
     file = open(path, "r")
-    for m in file.read().split("_"):
-        x.append(int(m.split('-')[0]))
-        r = m.split("-")[1].split("/")
-        y.append(float(r[0]) * 10**unit /float(r[1]))
+    for m in file.read().split("\n"):
+        if m != "":
+            x.append(int(m.split('-')[0]))
+            y.append(float(m.split('-')[1]))
     return np.array([np.array(x), np.array(y)])
 
 
@@ -61,7 +54,7 @@ def graph(coord, name, xlabel, ylabel="Temps d'exécution moyen (en $ms$)"):
 
 def reg(coord):
     slope, intercept, r, p_value, std_err = stats.linregress(coord[0], coord[1])
-    graph(coord, "Mesures", "Longueur du mot de tresse")
+    graph(coord, "", "")
     plt.plot(np.linspace(coord[0][0], coord[0][-1]), slope*np.linspace(coord[0][0], coord[0][-1])+intercept, label="Régression")
     print(str(slope)+"x"+str(intercept))
     print(r**2)
@@ -70,11 +63,14 @@ def reg(coord):
 #plt.style.use("Solarize_Light2")
 
 
-measure([True, 20, 91, 10])
+measure([
+    ["test/UF020.91/uf20-0", 100, 91]
+    ])
 
 c = coord_from_file("measure.txt")
-graph(c, "Temps d'exécution moyen", "Nombre de litéraux")
+graph(c, "Naive solver", "Nombre de litéraux")
 
+plt.title("Comparaison des satsolvers")
 plt.legend()
 plt.show()
 
