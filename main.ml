797642cb5a46ef0c0b9b2d0ed4892ee027e908f9
gaspardousson <gsapardousson>
2021-10-03 09:32:28 +0200

    Add results verification on "bool" measures

--- main.ml
+++ main.ml
@@ -1,14 +1,19 @@
 open Translater;;
 open Satsolver;;
 
+let solver = naive_solver;;
+
 let bool path n =
     let prob = Array.make n [||] in
     for i = 1 to n do
         prob.(i-1) <- read_cnf(path ^ (string_of_int i) ^ ".cnf")
     done;
     for i = 0 to n-1 do
-        print_endline (string_of_bool (naive_solver (prob.(i))))
-    done;;
+        if solver (prob.(i))
+            then print_endline "sat"
+            else print_endline "unsat"
+    done
+;;
 
 let time path n =
     let prob = Array.make n [||] in
@@ -17,17 +22,21 @@ let time path n =
     done;
     let t0 = Sys.time () in
     for i = 0 to n-1 do
-        ignore (naive_solver (prob.(i)))
+        ignore (solver (prob.(i)))
     done;
     let t1 = Sys.time () in
-    print_endline (string_of_float ((t1-.t0)/.(float_of_int n)));;
+    print_endline (string_of_float ((t1-.t0)/.(float_of_int n)))
+;;
 
 let () =
-    let path = Sys.argv.(1) in
-    let n = int_of_string Sys.argv.(2) in
-    let quantity = Sys.argv.(3) in
-    match quantity with
-        |"bool" -> bool path n
-        |"time" -> time path n
-        |_ -> failwith "Invalid quantity"
+    if Array.length Sys.argv = 4 then
+    begin
+        let path = Sys.argv.(1) in
+        let n = int_of_string Sys.argv.(2) in
+        let quantity = Sys.argv.(3) in
+        match quantity with
+            |"bool" -> bool path n
+            |"time" -> time path n
+            |_ -> failwith "Invalid quantity";
+    end
 ;;
--- measure.py
+++ measure.py
@@ -1,13 +1,12 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 
 
 def print_progress(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r"):
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + ' ' * (length - filledLength)
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + ' ' * (length - filled_length)
     print(f'\r{prefix} |{bar}| {percent}% {suffix}', end=print_end)
     if iteration == total:
         print()
@@ -21,17 +20,28 @@ def measure(files, quantity):
     for f in files:
         print_progress(k, len(files), prefix='Progress:', suffix='Complete', length=50)
         os.system("./satsolver-opt " + f[0] + " " + str(f[1]) + " " + quantity + " >> measures.txt")
-        print_progress(len(files), len(files), prefix='Progress:', suffix='Complete', length=50)
+        k += 1
+    print_progress(k, len(files), prefix='Progress:', suffix='Complete', length=50)
 
 
-def coord_from_file(path, taken=0, unit=6):
-    x, y = [], []
+def coord_from_file(path):
+    x, y = [0], [0]
+    file = open(path, "r")
+    m = file.read().split("\n")
+    for i in range(len(m)):
+        if m[i] != "":
+            x.append(files[i][2])
+            y.append(float(m[i]))
+    return np.array([np.array(x), np.array(y)])
+
+
+def bool_from_file(path):
+    b = []
     file = open(path, "r")
     for m in file.read().split("\n"):
         if m != "":
-            x.append(int(m.split('-')[0]))
-            y.append(float(m.split('-')[1]))
-    return np.array([np.array(x), np.array(y)])
+            b.append(m == "sat")
+    return b
 
 
 def graph(coord, name, xlabel, ylabel="Temps d'exécution moyen (en $ms$)"):
@@ -41,9 +51,10 @@ def graph(coord, name, xlabel, ylabel="Temps d'exécution moyen (en $ms$)"):
     plt.plot(x, y/1000, label=name)
 
 files = [
-    ["test/UF020.91/uf20-0", 100, 91]
+    ["test/UF020.91/uf20-0", 100, 20, True]
 ]
 quantity = "bool"
+plt.style.use("Solarize_Light2")
 
 measure(files, quantity)
 
@@ -51,8 +62,19 @@ if quantity == "time":
     c = coord_from_file("measures.txt")
     graph(c, "Naive solver", "Nombre de litéraux")
 
-    plt.style.use("Solarize_Light2")
     plt.title("Comparaison des satsolvers")
     plt.legend()
     plt.show()
 
+if quantity == "bool":
+    b = bool_from_file("measures.txt")
+    test = True
+    i = 0
+    for f in files:
+        for j in range(1, f[1]+1):
+            if b[i] != f[3]:
+                test = False
+                print("\33[101m" + "Solver output doesn't match expected result on " + f[0] + str(j))
+            i += 1
+    if test:
+        print("\33[102m" + "Test successfully passed!")
